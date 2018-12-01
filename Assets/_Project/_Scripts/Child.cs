5450185fe24950b706c037e5ec7a00fcf2ed9a47
Ivan Garcia Filho <ivan.garcia.filho@gmail.com>
2018-12-01 11:13:41 -0200

    base map setup

---
+++ Assets/_Project/_Scripts/Child.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Child : MonoBehaviour
+{
+	public Transform target;
+	public NavMeshAgent agent;
+	
+    void LateUpdate()
+    {
+		if(agent.remainingDistance <= 0.1)
+		{        
+			float dist = (target.position - transform.position).magnitude;
+			if(dist < 7)
+				agent.SetDestination(target.position);
+		}
+    }
+}
---
+++ Assets/_Project/_Scripts/Flautist.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flautist : MonoBehaviour
+{
+    public Rigidbody rb;
+	public float speed;
+	
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+		float dt = Time.deltaTime;
+		
+		Transform camTransform = Camera.main.transform.parent;
+		
+        float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		
+
+		Vector3 velocity = (v * camTransform.forward + h * camTransform.right);
+		velocity.y = rb.velocity.y;
+		
+		
+		rb.velocity = velocity * speed * dt;
+    }
+}
