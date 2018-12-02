17b1a57b34f1ee8200aba08589515e7ce7579de6
Ivan Garcia Filho <ivan.garcia.filho@gmail.com>
2018-12-02 01:04:30 -0200

    Improved game manager and Loader to load things in a more safe order and between better delays

--- Assets/_Project/_Scripts/GameManager.cs
+++ Assets/_Project/_Scripts/GameManager.cs
@@ -5,72 +5,77 @@ using UnityEngine.AI;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance = null;
+	public static GameManager Instance = null;
 
-    public GameObject childPrefab;
-    public Transform[] childGroupPoints;
-    public Vector2Int minMaxChildsPerGroup;
-    public Transform flautistTransform;
+	public GameObject childPrefab;
+	public Transform[] childGroupPoints;
+	public Vector2Int minMaxChildsPerGroup;
+	public Transform flautistTransform;
 	public Transform cauldronTransform;
 	public float cauldronRange = 18.0f;
+	public GameObject audioHyerarchy;
 
-    public PlayerController Player;
+	public PlayerController Player;
 	public float playerRange = 5;
 
-    void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
+	void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
 
-        else if (Instance != this)
-        {
-            Destroy(gameObject);
-        }
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
 
-        // Dont destroy on reloading the scene
-        DontDestroyOnLoad(gameObject);
+		// Dont destroy on reloading the scene
+		DontDestroyOnLoad(gameObject);
 
-        for (int i = 0; i < childGroupPoints.Length; i++)
-        {
-            SpawnChildsOnPoint(childGroupPoints[i], i);
-        }
-    }
+		for (int i = 0; i < childGroupPoints.Length; i++)
+		{
+			SpawnChildsOnPoint(childGroupPoints[i], i);
+		}
 
-    private void LateUpdate()
-    {
-        
-    }
+		StopAllCoroutines();
+		StartCoroutine(DelayedActivation(audioHyerarchy));
+	}
 
-    void SpawnChildsOnPoint(Transform t, int index)
-    {
-        int childCount = Random.Range(minMaxChildsPerGroup.x, minMaxChildsPerGroup.y);
-        Vector3 p = t.position;
+	private IEnumerator DelayedActivation(GameObject delayedObject, float delay = 5f)
+	{
+		yield return new WaitForSeconds(delay);
+		delayedObject.SetActive(true);
+	}
 
-        for (int i = 0; i < childCount; i++)
-        {
-            Vector3 pos = new Vector3(p.x + Random.Range(-4, 4), p.y, p.z + Random.Range(-4, 4));
+	void SpawnChildsOnPoint(Transform t, int index)
+	{
+		int childCount = Random.Range(minMaxChildsPerGroup.x, minMaxChildsPerGroup.y);
+		Vector3 p = t.position;
 
-            Child child = Instantiate(childPrefab, pos, Quaternion.identity).GetComponent<Child>();
+		for (int i = 0; i < childCount; i++)
+		{
+			Vector3 pos = new Vector3(p.x + Random.Range(-4, 4), p.y, p.z + Random.Range(-4, 4));
+
+			Child child = Instantiate(childPrefab, pos, Quaternion.identity).GetComponent<Child>();
 			child.transform.SetParent(t);
 			child.parentTransformIndex = index;
-			
-            child.target = flautistTransform;
-        }
-    }
-	
+
+			child.target = flautistTransform;
+		}
+	}
+
 	public void ReplaceChild(Child child)
 	{
 		Vector3 p = childGroupPoints[child.parentTransformIndex].position;
 		Vector3 pos = new Vector3(p.x + Random.Range(-4, 4), p.y, p.z + Random.Range(-4, 4));
-		
+
 		child.target = flautistTransform;
 		child.transform.position = pos;
 		child.gameObject.SetActive(true);
 	}
-   
-   	void OnDrawGizmos()
+
+	void OnDrawGizmos()
 	{
 		if(cauldronTransform)
 		{
--- Assets/_Project/_Scripts/Loader.cs
+++ Assets/_Project/_Scripts/Loader.cs
@@ -5,21 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class Loader : MonoBehaviour
 {
-    void Awake()
-    {
-        StartCoroutine(LoadAll());
-    }
-	
+	void Awake()
+	{
+		StartCoroutine(LoadAll());
+	}
+
 	IEnumerator LoadAll()
 	{
-		AsyncOperation sampleScene = SceneManager.LoadSceneAsync("SampleScene", LoadSceneMode.Additive);
-		while (sampleScene.isDone == false)
-        {
-            yield return null;
-        }
-		
-        AsyncOperation gameplayScene = SceneManager.LoadSceneAsync("Gameplay", LoadSceneMode.Additive);
-		
+		Time.timeScale = 0;
+
+		Application.backgroundLoadingPriority = ThreadPriority.BelowNormal;
+
+		AsyncOperation scenarioScene = SceneManager.LoadSceneAsync("Scenario", LoadSceneMode.Additive);
+		scenarioScene.allowSceneActivation = false;
+
+		while (scenarioScene.isDone == false)
+		{
+			yield return null;
+			if (scenarioScene.progress >= 0.9)
+			{
+				scenarioScene.allowSceneActivation = true;
+			}
+		}
+
+		AsyncOperation gameplayScene = SceneManager.LoadSceneAsync("Gameplay", LoadSceneMode.Additive);
+
+		while (gameplayScene.isDone == false)
+		{
+			yield return null;
+			if (gameplayScene.progress >= 0.9)
+			{
+				gameplayScene.allowSceneActivation = true;
+			}
+		}
+
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName("Scenario"));
+
 		SceneManager.UnloadSceneAsync("Loader");
+
+		Time.timeScale = 1;
 	}
 }
